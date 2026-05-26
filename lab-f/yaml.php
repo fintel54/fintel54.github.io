ea436a9a87bbf9bc249a1f186b367ec8062ec2e4
Michał Zając <zm59625@zut.edu.pl>
2026-05-26 20:32:52 +0200

    Update lab-f tasks and add gitignore

--- lab-f/yaml.php
+++ lab-f/yaml.php
@@ -1,1 +1,11 @@
-<?php
+<?php // I:\ptw\lab-f\yaml.php
+
+$data = [
+    'name' => 'Artur Karczmarczyk',
+    'index' => '3346',
+    'date' => date(DATE_ATOM),
+];
+
+$yaml = yaml_emit($data);
+
+echo $yaml;
